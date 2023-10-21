b9ae78c5bcec7bc097adfc0fd1e532ce7293690c
kaan177 <66557991+kaan177@users.noreply.github.com>
2023-10-21 21:02:21 +0200

    fix lol

--- Tetris - Code template/TetrisTemplate/TetrisGrid.cs
+++ Tetris - Code template/TetrisTemplate/TetrisGrid.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 
